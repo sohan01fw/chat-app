d899ef6823cb039486a147c4519f569a96df947e
sohancwact <sohanshiwakoti1@gmail.com>
2022-12-26 16:59:55 +0545

    .gitignore is now working

--- chatter_server/routes/messageRoute.js
+++
@@ -1,6 +0,0 @@
-const { addMsg, getMsg } = require("../controllers/messageController");
-
-const Msgrouter = require("express").Router();
-Msgrouter.post("/addMsg", addMsg);
-Msgrouter.post("/getMsg", getMsg);
-module.exports = Msgrouter;
